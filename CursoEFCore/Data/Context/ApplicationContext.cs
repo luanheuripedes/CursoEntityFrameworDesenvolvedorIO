1cadc96f9e9270e8c5b9724352534fd0c0990f3e
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-03-09 14:19:10 -0300

    Terminado de fazer as classes de configuração das entidades e mostrado duas formas de fazer com que a classe context execute as classes de configuração

---
+++ CursoEFCore/Data/Context/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using CursoEFCore.Data.Configurations;
+using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CursoEFCore.Context
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Database=EfCoreIntroducao;Uid=root;Pwd=123456789;",
+                                    new MySqlServerVersion(new Version(10, 4, 17)));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Adiciona as configurações feitas separadas na hora de gerar o banco
+            //modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+
+            //Adiciona as configurações feitas separadas na hora de gerar o banco
+            modelBuilder.ApplyConfigurationFromAssembly(typeof(ApplicationContext).Assembly);
+
+
+        }
+    }
+}
